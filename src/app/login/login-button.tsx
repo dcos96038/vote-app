0dfe4b68e773ad869c9ca115fff535169282fdc2
Diego Coscolla <diegocoscolla@gmail.com>
2024-03-25 22:23:39 -0300

    Add LoginButton component to Login page

---
+++ src/app/login/login-button.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/providers/auth-provider";
+import { IconBrandGoogle } from "@tabler/icons-react";
+
+export const LoginButton = () => {
+  const auth = useAuth();
+
+  return (
+    <Button className="w-full" onClick={auth.loginWithGoogle}>
+      <IconBrandGoogle className="mr-2" stroke={2} /> Login with Google
+    </Button>
+  );
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,12 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useAuth } from "@/providers/auth-provider";
-import { IconBrandGoogle } from "@tabler/icons-react";
+import { LoginButton } from "./login-button";
 
 export default function Login() {
-  const auth = useAuth();
-
   return (
     <main className="mx-auto flex min-h-screen max-w-screen-lg items-center justify-center">
       <div className="flex min-w-96 flex-col gap-10 rounded-md bg-slate-800 px-10 py-4">
         <h1 className="text-center text-xl font-medium">Login with socials</h1>
-        <div className="">
-          <Button className="w-full" onClick={auth.loginWithGoogle}>
-            <IconBrandGoogle className="mr-2" stroke={2} /> Login with Google
-          </Button>
+        <div>
+          <LoginButton />
         </div>
       </div>
     </main>

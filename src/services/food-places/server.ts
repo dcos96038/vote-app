a71de9d51fd446c197f75fe5980de41cfbae1b20
Diego Coscolla <diegocoscolla@gmail.com>
2024-03-30 15:21:16 -0300

    Refactor getFoodPlaces to getVerifiedFoodPlaces

--- src/services/food-places/server.ts
+++ src/services/food-places/server.ts
@@ -1,8 +1,12 @@
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
-export const getFoodPlaces = async () => {
+export const getVerifiedFoodPlaces = async () => {
   const supabase = createSupabaseServerClient();
-  const response = await supabase.from("food_places").select("*").filter("verified", "eq", true)
+  const response = await supabase
+    .from("food_places")
+    .select("*")
+    .filter("verified", "eq", true);
+
   if (response.error) {
     throw response.error;
   }

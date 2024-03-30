f32498cf931e3fa25688a5f64ef000ca0677a51a
Diego Coscolla <diegocoscolla@gmail.com>
2024-03-30 15:21:32 -0300

    Update getFoodPlaces to getVerifiedFoodPlaces

--- src/app/(authenticated)/top/page.tsx
+++ src/app/(authenticated)/top/page.tsx
@@ -1,8 +1,8 @@
 import { TopTable } from "@/components/top-table";
-import { getFoodPlaces } from "@/services/food-places/server";
+import { getVerifiedFoodPlaces } from "@/services/food-places/server";
 
 async function TopPage() {
-  const foodPlaces = await getFoodPlaces();
+  const foodPlaces = await getVerifiedFoodPlaces();
 
   return (
     <div>

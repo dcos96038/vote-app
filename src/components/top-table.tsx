732795285c7d5d8bf025f5e5bdfa8e9d5691c091
Diego Coscolla <diegocoscolla@gmail.com>
2024-03-30 14:53:16 -0300

    Update top-table component and food-places server API

--- src/components/top-table.tsx
+++ src/components/top-table.tsx
@@ -9,7 +9,7 @@ import {
 import { cn } from "@/lib/utils";
 import { Database } from "@/types/global";
 
-const COLUMNS = ["Name", "Location", "Rating", "Working Hours", "Menu"];
+const COLUMNS = ["Nombre", "Ubicación", "Horarios", "Menú"];
 
 interface TopTableProps {
   data: Database["public"]["Tables"]["food_places"]["Row"][];
@@ -23,7 +23,7 @@ export function TopTable({ data }: TopTableProps) {
           {COLUMNS.map((colName, i) => (
             <TableHead
               key={colName}
-              className={cn({
+              className={cn("bg-slate-700/50",{
                 "w-[200px]": i === 0,
                 "text-right": i === COLUMNS.length - 1,
               })}
@@ -34,17 +34,22 @@ export function TopTable({ data }: TopTableProps) {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {data.map((business) => (
+        {data.length > 0 ? data.map((business) => (
           <TableRow key={business.name}>
             <TableCell className="font-medium">{business.name}</TableCell>
             <TableCell>{business.location}</TableCell>
-            <TableCell>{business.score ?? 0}</TableCell>
             <TableCell>
               {business.opening_time} - {business.closing_time}
             </TableCell>
             <TableCell className="text-right">{business.menu ?? "-"}</TableCell>
           </TableRow>
-        ))}
+        )) : (
+          <TableRow>
+            <TableCell colSpan={COLUMNS.length} className="text-center">
+              No hay locales disponibles
+            </TableCell>
+          </TableRow>
+        )}
       </TableBody>
     </Table>
   );
--- src/services/food-places/server.ts
+++ src/services/food-places/server.ts
@@ -2,7 +2,7 @@ import { createSupabaseServerClient } from "@/lib/supabase/server";
 
 export const getFoodPlaces = async () => {
   const supabase = createSupabaseServerClient();
-  const response = await supabase.from("food_places").select("*");
+  const response = await supabase.from("food_places").select("*").filter("verified", "eq", true)
   if (response.error) {
     throw response.error;
   }

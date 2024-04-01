c800ea5f1c9adaa0de6e2ac9aaafdf985abbb194
Sergio Rubino <srg.rbn7@gmail.com>
2024-04-01 18:25:07 -0300

    Update voting functionality

--- src/services/weekly-votes.ts
+++ src/services/weekly-votes.ts
@@ -1,15 +1,17 @@
-import { InsertWeeklyVote } from "@/types/globals";
-import { Database } from "@/types/supabase";
-import { SupabaseClient } from "@supabase/supabase-js";
+import { InsertWeeklyVote, SupabaseClient } from "@/types/globals";
 
 export class WeeklyVotesService {
   private readonly table;
 
-  constructor(private readonly client: SupabaseClient<Database, "public">) {
+  constructor(private readonly client: SupabaseClient) {
     this.table = this.client.from("weekly_votes");
   }
 
   async insert({ place, meet, user }: InsertWeeklyVote) {
     await this.table.insert({ place, meet, user });
+  }
+
+  async update({ place, meet, user }: InsertWeeklyVote) {
+    await this.table.update({ place }).match({ user, meet });
   }
 }
